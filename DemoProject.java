130b88f48bc7b8abfc61ab9d96c72fe56a8f5d84
Raynesha Lucas <rdaniels08@gmail.com>
2022-11-21 21:06:39 -0500

    Update
    removed lines of code

--- DemoProject.java
+++ DemoProject.java
@@ -4,8 +4,7 @@ public class DemoProject {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
-		System.out.println("My name is Raynesha!!!");
-		System.out.println("how are you doing?");
+
 	}
 
 }
